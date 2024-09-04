61c7fd53e72da06b512cada48837efc7d4bd6eea
AlexandreCalmon <alexandrecalmonjunior@gmail.com>
2024-09-04 20:47:12 -0300

    feat javascript dados.js

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Dart",
+        descricao: "Dart é uma linguagem de programação versátil e moderna...",
+        link: "https://flutter.dev/"
+    },
+    {
+        titulo: "Python",
+        descricao: "Python é uma linguagem de programação de alto nível, conhecida por sua sintaxe clara e legível. É amplamente utilizada em diversas áreas, como ciência de dados, machine learning e desenvolvimento web.",
+        link: "https://www.python.org/"
+    },
+
+    {
+        titulo: "JavaScript",
+        descricao: "JavaScript é uma linguagem de programação interpretada, principalmente utilizada para criar interatividade em páginas web. É a linguagem padrão para desenvolvimento front-end e também pode ser usada no back-end com Node.js.",
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+    }
+];
